e585b7dfe141632a8f5eecc6e4f595810cbd8e50
Kim Ji Woo <jiwoo22110103@gmail.com>
2026-07-07 14:27:04 +0900

    refactor(runtime): export canonical state projection API

--- harness_codex/runtime/_public_state.py
+++ harness_codex/runtime/_public_state.py
@@ -21,12 +21,24 @@ from harness_codex.runtime.state import (
     stage_artifact_notes,
     stage_artifact_status,
 )
+from harness_codex.runtime.state_projection import (
+    STATE_SCHEMA_VERSION,
+    canonical_work_item_states,
+    canonicalize_run_state,
+    dashboard_projection,
+    load_dashboard_projections,
+    migrate_legacy_runtime_state,
+    persist_canonical_run_state,
+)
 
 __all__ = [
     "ArtifactDirtyState", "ArtifactManifest", "MaintenanceStep", "ReportWriter",
     "ResumeDisposition", "ResumeTarget", "RunFailureKind", "RunReport", "RunState",
     "RunStateStore", "StageArtifactState", "StageStateDrift", "UseCaseLoopState",
     "UseCaseReport", "UseCaseStep", "WorkItemLoopState", "WorkItemReport",
-    "decide_resume_target", "file_checksum", "reconcile_procedure_stage_rows",
-    "runtime_stage_projection", "stage_artifact_notes", "stage_artifact_status",
+    "STATE_SCHEMA_VERSION", "canonical_work_item_states", "canonicalize_run_state",
+    "dashboard_projection", "load_dashboard_projections", "migrate_legacy_runtime_state",
+    "persist_canonical_run_state", "decide_resume_target", "file_checksum",
+    "reconcile_procedure_stage_rows", "runtime_stage_projection", "stage_artifact_notes",
+    "stage_artifact_status",
 ]
